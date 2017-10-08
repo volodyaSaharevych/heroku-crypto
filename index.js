63755daf7d742577368ff96b73813686237789c7
Volodymyr <volodyasaharevych@gmail.com>
2017-10-08 23:02:46 +0300

    change

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var corsOptions = {
 };
 
 app.listen( process.env.PORT || 8080 );
-
 app.use( function ( req, res, next ) {
     res.header( 'Access-Control-Allow-Origin', '*' );
     res.header( 'Access-Control-Allow-Credentials', true );
