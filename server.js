fec7fcef85de0a669d37affb730d4e54535208c3
Volodymyr <volodyasaharevych@gmail.com>
2017-10-08 14:25:07 +0300

    crypto

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const md5 = require('./node_modules/crypto-js');
 const parsBodyPost = require('body-parser');
-
 // app.listen( process.env.PORT || 8080 );
-
 app.use( parsBodyPost.json() ); 
 app.use( parsBodyPost.urlencoded({ extended: true }) ); 
 
