4c35842b4564aa508e29468a788179c078cd0738
Volodymyr <volodyasaharevych@gmail.com>
2017-10-10 11:33:36 +0300

    clear index

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-var express = require('express');
-var app = express();
-var md5 = require('crypto-js');
+var express      = require('express');
+var app          = express();
+var md5          = require('crypto-js');
 var parsBodyPost = require('body-parser');
-var rout = express.Router();
-var cors = require('cors');
-var corsOptions = {
-    origin  : 'http://freeman.org.ua/',
-    methods : ['POST'],
-    optionsSuccessStatus : 200 
-};
+var cors         = require('cors');
 
 app.listen( process.env.PORT || 8080 );
-app.use( function ( req, res, next ) {
-    res.header( 'Access-Control-Allow-Origin', '*' );
-    res.header( 'Access-Control-Allow-Credentials', true );
-    res.header( 'Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS' );
-    res.header( 'Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json' );
-    next();
-});
 
 app.use( parsBodyPost.json() ); 
-app.use(cors());
+app.use( cors() );
 app.use( parsBodyPost.urlencoded({ extended: true }) ); 
 app.post( '/crypto/', ( req, res, next ) => {
     let product = req.body.product;
-    let h = md5.HmacMD5( product, '33928d837fbb80bf876718623ad70925404f9f76').toString();
+    let secret_key = '33928d837fbb80bf876718623ad70925404f9f76';
+    let h = md5.HmacMD5( product, secret_key ).toString();
     res.send( h );
 });
 
