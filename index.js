11efbfd4c0c1a1e5e81f63f34ad67eda48dced95
Volodymyr <volodyasaharevych@gmail.com>
2018-09-27 15:49:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen( process.env.PORT );
 app.use( parsBodyPost.json() ); 
 app.use( cors() );
 app.use( parsBodyPost.urlencoded({ extended: true }) ); 
-app.post( '/crypto/', ( req, res, next ) => {
+app.post( '', ( req, res, next ) => {
     let product = req.body.product;
     let secret_key = '33928d837fbb80bf876718623ad70925404f9f76';
     let h = md5.HmacMD5( product, secret_key ).toString();
